62237142977c7f048b1af3c43a29bfc691cb56ab
Nazarsten <nazar.sten849@ukr.net>
2021-11-11 20:14:35 +0200

    fix_errors

---
+++ fix.errors.js
@@ -0,0 +1,14 @@
+let a = 100;
+let b = 500;
+let c = "hello";
+let d = "okten";
+let x = 'constant value';
+{
+    console.log(a);
+    x = 'new value';
+}
+b = 'new value';
+let y = 10;
+let result = 100 + y;
+
+console.log(result);
